ddcc0ddbe1b8f4b604998d462f553cbf34da7302
MarKraken <110046283+mariasmoUN@users.noreply.github.com>
2024-07-31 22:11:35 -0500

    the end

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var bodyHidden = document.getElementById("body");
+
 /* Sonido de fondo */
 function PlayAudio() {
   document.getElementById("background-music").play();
@@ -7,7 +9,6 @@ function PlayAudio() {
 document.getElementById("btn-play").addEventListener("click", () => {
   const contenedorLoader = document.querySelector(".container--loader");
   const cover = document.querySelector(".video_cover");
-  var bodyHidden = document.getElementById("body");
 
   contenedorLoader.style.opacity = 0;
   contenedorLoader.style.visibility = "hidden";
@@ -55,3 +56,25 @@ const countdownInterval = setInterval(function () {
     document.getElementById("countdown").innerHTML = "¡Llegó el momento!";
   }
 }, 1000);
+
+/* Galery */
+document.addEventListener("DOMContentLoaded", function () {
+  const thumbnails = document.querySelectorAll(".thumbnail");
+  const overlay = document.getElementById("overlay");
+  const overlayImg = document.getElementById("overlayImg");
+  const closeBtn = document.getElementById("closeBtn");
+
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.addEventListener("click", function () {
+      overlay.style.display = "flex";
+      overlayImg.style.backgroundImage = this.style.backgroundImage;
+      bodyHidden.style.overflow = "hidden";
+    });
+  });
+
+  closeBtn.addEventListener("click", function () {
+    overlay.style.display = "none";
+    overlayImg.style.backgroundImage = "";
+    bodyHidden.style.overflow = "auto";
+  });
+});
